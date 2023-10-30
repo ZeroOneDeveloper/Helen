b1a636d87251bd5c4072af30dc9b87a3fc13f0c8
paring <paring@pikokr.dev>
2023-10-30 20:34:17 +0900

    feat: search

--- app/dashboard/videos/page.tsx
+++ app/dashboard/videos/page.tsx
@@ -4,12 +4,12 @@ import * as React from 'react'
 
 import { VirtuosoGrid } from 'react-virtuoso'
 import { PostgrestError } from '@supabase/supabase-js'
-import { Box, Container, Spinner, styled } from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, Box, Container, Input, InputGroup, InputLeftAddon, Spinner } from '@chakra-ui/react'
 
 import { Video } from '@/types/data'
 import { supabaseClient } from '@utils/supabaseClient'
 import { VideoListCard } from '@components/video/VideoListCard'
-import { VideoCardSkeleton } from '@components/video/VideoCardSkeleton'
+import { TbSearch } from 'react-icons/tb'
 
 const ListContainer = React.forwardRef<HTMLDivElement, React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>>(({ children, ...props }, ref) => (
 	<Box display="grid" flexWrap="wrap" gridTemplateColumns="repeat(3, 1fr)" gap={4} ref={ref} {...props}>
@@ -21,51 +21,75 @@ const VideoListPage: React.FC = () => {
 	const [total, setTotal] = React.useState(0)
 	const [error, setError] = React.useState<PostgrestError | null>(null)
 	const [data, setData] = React.useState<Video[]>([])
-	const hasMore = React.useMemo(() => total === 0 || data.length < total, [total, data])
+	const [query, setQuery] = React.useState('')
+	const [loading, setLoading] = React.useState(true)
+	const hasMore = React.useMemo(() => data.length < total, [total, data])
 
 	const supabase = supabaseClient()
 
-	const fetchItems = async (offset: number) => {
-		const result = await supabase
+	const fetchItems = async (offset: number, reset: boolean = false, abort: AbortSignal | null = null) => {
+		let queryBuilder = supabase
 			.from('videos')
 			.select('*', { count: 'exact' })
 			.range(offset, offset + 49)
+
+		if (query) queryBuilder = queryBuilder.textSearch('title', query)
+
+		if (abort) queryBuilder = queryBuilder.abortSignal(abort)
+		const result = await queryBuilder
+
 		if (result.error != null) {
 			setError(result.error)
 			return
 		}
 		setError(null)
-		setData((data) => [...data, ...result.data])
+		if (reset) setData(result.data)
+		else setData((data) => [...data, ...result.data])
 		setTotal(result.count!)
 	}
 
 	React.useEffect(() => {
+		const ac = new AbortController()
 		const timeout = setTimeout(() => {
-			console.log('fetch first')
-			fetchItems(0)
-		})
+			setLoading(true)
+			setData([])
+			fetchItems(0, true, ac.signal).finally(() => setLoading(false))
+		}, 300)
 
-		return () => clearTimeout(timeout)
-	}, [])
+		return () => {
+			clearTimeout(timeout)
+			ac.abort()
+		}
+	}, [query])
 
 	return (
 		<Container maxW="container.lg">
+			<InputGroup>
+				<InputLeftAddon>
+					<TbSearch />
+				</InputLeftAddon>
+				<Input mb={4} onChange={(e) => setQuery(e.target.value)} value={query} placeholder="검색" />
+			</InputGroup>
 			<VirtuosoGrid
 				useWindowScroll
 				components={{
 					List: ListContainer,
-					ScrollSeekPlaceholder: () => <VideoCardSkeleton />,
 				}}
 				overscan={200}
 				totalCount={data.length}
-				endReached={() => fetchItems(data.length)}
-				itemContent={(i) => <VideoListCard video={data[i]} />}
-				scrollSeekConfiguration={{
-					enter: (velocity) => Math.abs(velocity) > 200,
-					exit: (velocity) => Math.abs(velocity) < 30,
+				endReached={() => {
+					setLoading(true)
+					fetchItems(data.length).finally(() => setLoading(false))
 				}}
+				itemContent={(i) => <VideoListCard video={data[i]} />}
 			/>
-			{hasMore && (
+			{data.length === 0 && !loading && !hasMore && (
+				<Alert>
+					<AlertIcon />
+					<AlertTitle>검색 결과가 없습니다</AlertTitle>
+				</Alert>
+			)}
+			{loading && hasMore && (
 				<Box w="full" display="flex" justifyContent="center" mt={4}>
 					<Spinner />
 				</Box>
